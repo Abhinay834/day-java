2e559e323f05778c63d7999d8b18294db79f2489
bobby12312 <104480010+bobby12312@users.noreply.github.com>
2022-05-27 22:03:38 -0600

    Create line comparision.java

---
+++ line comparision.java
@@ -0,0 +1,12 @@
+
+    public static void checkEquality(double lineOneLength , double lineTwoLength ) {
+        if(lineOneLength == lineTwoLength ) {
+        if (lineOneLength == lineTwoLength) {
+            System.out.println("Lines are Equal");
+        }
+        else if(lineOneLength > lineTwoLength) {
+            System.out.println("line1 is greater than line2");
+        }
+        else {
+            System.out.println("Lines are not Equal");
+        }
